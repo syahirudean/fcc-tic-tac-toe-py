14a554f6f095208b7f4b561664398581f6b60480
Syahiruddin Daud <syahiruddin.daud@gmail.com>
2021-10-17 17:40:47 +0800

    feat(AI): implemented-AI

--- player.py
+++ player.py
@@ -38,8 +38,74 @@ class HumanPlayer(Player):
                 val = int(square)
                 if val not in game.available_moves():
                     raise ValueError
-                valid_square  = True # if these are successful, then yay!
-            except  ValueError:
+                valid_square = True  # if these are successful, then yay!
+            except ValueError:
                 print('Invalid square. Try again.')
 
         return val
+
+
+class GeniusComputerPlayer(Player):
+    def __init__(self, letter):
+        super().__init__(letter)
+
+    def get_move(self, game):
+        if len(game.available_moves()) == 9:
+            # randomly choose one
+            square = random.choice(game.available_moves())
+        else:
+            # get the square based off the minimax algorithm
+            square = self.minimax(game, self.letter)['position']
+        return square
+
+    def minimax(self, state, player):
+        max_player = self.letter  # yourself!!!
+        # the other player.. so whatever letter is NOT u
+        other_player = '0' if player == 'X' else 'X'
+
+        # first, we want to check if the previous move is a winner
+        # this is our base case
+        if state.current_winner == other_player:
+            # we should return postition AND score because we nee to keep track of the score
+            # for the minimax to work
+            return {'position': None,
+                    'score': 1 * (state.num_empty_squares() + 1) if other_player == max_player else -1 * (
+                        state.num_empty_squares() + 1
+                    )}
+
+        elif not state.empty_squares():  # no empty squares
+            return {'position': None, 'score': 0}
+
+        # THE ALGORITHM
+        # initialize some dictionaries
+        if player == max_player:
+            # each score schould maximize (be larger)
+            best = {'position': None, 'score': -math.inf}
+        else:
+            # each score schould minimize
+            best = {'position': None, 'score': math.inf}
+
+        for possible_move in state.available_moves():
+            
+            # step 1: make a move, try that spot
+            state.make_move(possible_move, player)
+
+            # step 2: recurese using minimax to stimulate a game after making that move
+            # now, we alternate players
+            sim_score = self.minimax(state, other_player)
+
+            # step 3: undo the move
+            state.board[possible_move] = ' '
+            state.current_winner = None
+            # otherwise this will get messed up from the recursion
+            sim_score['position'] = possible_move
+
+            # step 4: update the dictionaries if necessary
+            if player == max_player:  # we are trying to minimiza the max_player
+                if sim_score['score'] > best['score']:
+                    best = sim_score  # replace best
+            else:  # but minimize the other player
+                if sim_score['score'] < best['score']:
+                    best = sim_score  # replace best
+
+        return best
